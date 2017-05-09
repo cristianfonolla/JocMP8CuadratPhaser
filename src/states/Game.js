0f5515caed166f10bae68b143e9b403b547f6de4
cristianfonolla <cristian.fonolla@gmail.com>
2017-05-09 21:51:30 +0200

    die donew

--- src/states/Game.js
+++ src/states/Game.js
@@ -5,42 +5,48 @@ import Player from '../sprites/Player'
 export default class extends Phaser.State {
   init () {}
   preload () {
-    game.stage.backgroundColor = '#EDEEC9'
+    game.stage.backgroundColor = 'blue'
     this.game.load.spritesheet('player', 'assets/player.png', 28, 22)
+
     this.game.load.image('ground','./assets/ground.png')
+
     this.game.load.image('wall','./assets/wall.png')
+
     this.game.load.image('enemy','./assets/enemy.png')
+
     this.game.load.image('coin','./assets/coin.png')
+
+    this.game.load.image('exp','./assets/exp.png')
+
+
     this.game.load.audio('jump',['./assets/jump.wav','./assets/jump.mp3'])
+    this.game.load.audio('dead',['./assets/dead.wav','./assets/dead.mp3'])
+    this.game.load.audio('coin',['./assets/coin.wav','./assets/coin.mp3'])
   }
 
   create () {
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
-    // this.player = this.game.add.sprite(350, 101, 'player')
-    this.player = new Player({
-        game: this.game,
-        x: 350,
-        y: 101,
-        asset: 'player'
-    })
-    this.game.add.existing(this.player)
+
+    this.spawnPlayer()
 
     this.loadlevel()
+
+    this.jumpSound = this.game.add.audio('jump')
+    this.coinSound = this.game.add.audio('coin')
+    this.deadSound = this.game.add.audio('dead')
 
     this.enemy = this.game.add.sprite(450,400/2-20,'enemy')
 
     this.putCoinsOnLevel()
 
-    this.jumpSound = this.game.add.audio('jump')
+    this.setParticles()
+
 
     game.physics.arcade.enable(this.player)
     game.physics.arcade.enable(this.enemy)
-
     this.player.body.gravity.y = 600
     this.player.body.setSize(20,20,0,0)
-
     this.player.animations.add('idle',[3,4,5,4],5,true)
-
     this.player.animations.play('idle')
 
     this.cursor = this.game.input.keyboard.createCursorKeys()
@@ -49,8 +55,8 @@ export default class extends Phaser.State {
 
   update () {
     this.game.physics.arcade.collide(this.player,this.level)
-    this.game.physics.arcade.overlap(this.player,this.coins, this.takeCoin)
-    this.game.physics.arcade.overlap(this.player,this.enemy)
+    this.game.physics.arcade.overlap(this.player,this.coins, this.takeCoin, null, this)
+    this.game.physics.arcade.overlap(this.player,this.enemy, this.die, null, this)
 
     this.inputs()
 
@@ -65,7 +71,7 @@ export default class extends Phaser.State {
 
   inputs () {
     if (this.cursor.left.isDown) {
-      this.player.body.velocity.x = -100 //TODO moure a la dreta + colisió amb les parets + inèrcia per parar
+      this.player.body.velocity.x = -100 
       this.player.frame = 2
     } else if(this.cursor.right.isDown) {
         this.player.body.velocity.x = 100
@@ -90,16 +96,18 @@ export default class extends Phaser.State {
   }
 
   takeCoin (player, coin) {
-    //TODO Reproduir coin
 
     console.log('moneda tocada')
     coin.body.enable = false
     game.add.tween(coin).to({width:0},100).start()
 
+    this.coinSound.play()
+
   }
 
   jumpPlayer () {
       if(!this.hasJumped) {
+        console.log(this.hasJumped)
         this.player.body.velocity.y = -220
         this.jumpSound.play()
         this.hasJumped = true
@@ -112,9 +120,44 @@ export default class extends Phaser.State {
     this.ground = this.game.add.sprite(760/2-160,400/2,'ground',0, this.level)
     this.wall1 = this.game.add.sprite(760/2-160,480/2-80,'wall',0, this.level)
     this.wall2 = this.game.add.sprite(760/2+140,400/2-80,'wall',0, this.level)
-    // game.physics.arcade.enable(this.level)
     this.level.setAll('body.immovable', true)
   }
+
+  die (player, enemy) {
+      game.camera.shake(0.05,200)
+      this.deadSound.play()
+      this.playerIsDead = true
+      this.explosion.x = this.player.x
+      this.explosion.y = this.player.y+10
+      this.explosion.start(true, 300, null, 20)
+      this.spawnPlayer()
+  }
+
+  setParticles () {
+    this.explosion = game.add.emitter(0,0,20)
+    this.explosion.makeParticles('exp')
+    this.explosion.setYSpeed(-150,150)
+    this.explosion.setXSpeed(-150,150)
+    // this.explosion.gravity.set(0,200)
+  }
+
+  spawnPlayer () {
+    if(this.playerIsDead){
+        this.player.x = 350
+        this.player.y = 101
+        this.playerIsDead = false
+    }else {
+        this.player = new Player({
+            game: this.game,
+            x: 350,
+            y: 101,
+            asset: 'player'
+        })
+        this.game.add.existing(this.player)
+    }
+  }
+
+
 
   render () {
     if (__DEV__) {
